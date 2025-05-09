c7716f1491ca9ab71995cdbc96f4b235640ded31
MaxiiYkz <max.paez@duocuc.cl>
2025-05-09 11:46:16 -0400

    Clase 09/05

--- src/main/java/com/example/Proyecto/model/Usuario.java
+++ src/main/java/com/example/Proyecto/model/Usuario.java
@@ -1,24 +1,39 @@
 package com.example.Proyecto.model;
 
 import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Table(name = "usuario")
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class Usuario {
     @Id
-    @Generated()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idUsuario;
 
+    @Column(unique = true, length = 13, nullable = false)
+    private String run;
+
+    @Column(nullable = false)
     private String usuario;
+
+    @Column(nullable = false)
     private String contraseña;
+
+    @Column(nullable = false)
     private String correo;
+
+    @Column(nullable = false)
     private int telefono;
 
 }
